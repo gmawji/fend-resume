bf97fcfd9a70fd22c3a3586909332ab83270733a
gmawji <git@gmawji.com>
2017-03-03 00:42:23 -0500

    Cleaned up formatting. Updated work obj Added education obj

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -1,43 +1,4 @@
-/* Try Append Quiz */
-// $("#main").append("Gulamabbas Mawji");
-
-/* Variables */
-//var awesomeThoughts = "I am Gulamabbas and I am AWESOME!";
-
-// var funThoughts =
-//	awesomeThoughts.replace("AWESOME", "FUN");
-
-// [string].replace([old],[new])
-
-// var email = "edu@gmail.com";
-
-// var newEmail =
-// 	email.replace("gmail", "gmawji");
-
-// $("#main").append(funThoughts);
-
- var formattedName = HTMLheaderName.replace("%data%", "Gulamabbas Mawji");
-
-var formattedRole = HTMLheaderRole.replace("%data%", "Front-End Web Developer");
-
-// var mobile = HTMLmobile.replace("%data%", "407-416-6842");
-// var email = HTMLemail.replace("%data%", "me@gmawji.com");
-
 var avatar = HTMLbioPic.replace("%data%", "images/bioPic.png");
-
-// var skills = HTMLskillsStart;
-// var skillone = HTMLskills.replace("%data%", "HTML");
-// var skilltwo = HTMLskills.replace("%data%", "CSS");
-
-// var welcomeMessage = HTMLwelcomeMsg.replace("%data%", "Welcome Msg");
-
-// $("#header").prepend(formattedRole);
-// $("#header").prepend(formattedName);
-
-// var skills =
-// ["HTML", "CSS","Python", "GitHub", "Management"];
-
-// $("#main").append(skills);
 
 var bio = {
 	"name" : formattedName,
@@ -45,7 +6,7 @@ var bio = {
 	"contacts" : {
 		"mobile" : "407-416-6482",
 		"email" : "dev@gmawji.com",
-		"twitter" : "@jokerg7",
+		"twitter" : "@gmawji7",
 		"location" : "Lake Mary, Florida",
 		"github" : "gmawji"
 	},
@@ -54,19 +15,56 @@ var bio = {
 	"skills" : ["HTML", "CSS","Python", "GitHub", "Management"]
 };
 
-bio.work = {
-	"position" : "Front-End Web Developer",
-	"employer" : "SimplyNAS",
-	"years" : 8 + "years",
-	"city" : "Sanford, FL"
-};
+var work = {
+	"jobs": [
+		{
+			"position" : "Web & Designer",
+			"employer" : "Freelance",
+			"years" : "4 Years",
+			"city" : "Sanford, FL"
+		},
+		{
+			"position" : "Co-Founder",
+			"employer" : "SimplyGroup Inc",
+			"years" : "8 Years",
+			"city" : "Sanford, FL"
+		},
+		{
+			"position" : "Systems Integrator",
+			"employer" : "eAegis LLC",
+			"years" : "2 Years",
+			"city" : "Longwood, FL"
+		}
+	]
+}
 
-bio["edu"] = {
-	"name" : "Lake Mary High School",
-	"years": 4 + "years",
-	"city" : "Lake Mary, FL"
-};
-
+var education = {
+	"schools": [
+		{
+			"name": "Lake Mary High School",
+			"years": "4 Years",
+			"city": "Lake Mary, FL",
+			"majors": ["High School Diploma"],
+			"graduation": "May 2007"
+		}
+	],
+	"onlineCourses": [
+		{
+			"name": "Udacity",
+			"years": "1 Year",
+			"city": "Worldwide",
+			"majors": ["Front-End Web Developer Nanodegree"],
+			"graduation": "Ongoing"
+		},
+		{
+			"name": "Udacity",
+			"years": "1 Year",
+			"city": "Worldwide",
+			"majors": ["Full Stack Web Developer Nanodegree"],
+			"graduation": "Ongoing"
+		}
+	]
+}
 
 
 $("#header").prepend(bio.role);
@@ -79,6 +77,6 @@ $("#header").append(bio.contacts.github);
 $("#header").append(bio.picture);
 $("#header").append(bio.welcome);
 $("#header").append(bio.skills);
-$("#header").append(bio.work.position);
-$("#header").append(bio.edu.name);
+$("#header").append(work.position);
+$("#header").append(edu.name);
 
