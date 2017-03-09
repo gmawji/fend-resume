ca016a3095236be8e9665eeb4f8204cdbc2b3465
gmawji <git@gmawji.com>
2017-03-09 12:40:57 -0500

    Added International format button. Not styled yet.

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -107,6 +107,19 @@ $("#header").append(HTMLbioPic.replace("%data%", bio.biopic));
 
 $("#header").append(HTMLwelcomeMsg.replace("%data%", bio.welcomeMessage));
 
+function inName(name) {
+
+	name = name.trim().split(" ");
+	console.log(name);
+	name[0] = name[0].slice(0,1).toUpperCase() + name[0].slice(1).toLowerCase();
+	name[1] = name[1].toUpperCase();
+
+	return name[0] +" "+ name[1];
+
+}
+
+$("#main").append(internationalizeButton);
+
 function displayWork() {
 
 	if(work.jobs.length > 0) {
