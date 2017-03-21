0bd4eb4ee73c8ad70b26365c20b1cbf0809540c4
gmawji <git@gmawji.com>
2017-03-21 13:39:22 -0400

    Let's Connect!

--- js/manipulation.js
+++ js/manipulation.js
@@ -7,3 +7,8 @@ function changeButton() {
   $('button').first().addClass("btn")
 }
 $(changeButton);
+
+function mailConnect() {
+  $('div.dark-gray').children('h2.orange.center-text').prepend('<a href = "mailto:signup@gmawji.com?Subject=Lets%20Connect" target="_top"> Let\'s Connect </a>');
+}
+$(mailConnect);
