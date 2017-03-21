83f45770f192137dc28fc88397247b04fcb49167
gmawji <git@gmawji.com>
2017-03-21 18:44:48 -0400

    Manipulating online classes to link them.

--- js/manipulation.js
+++ js/manipulation.js
@@ -12,3 +12,22 @@ function mailConnect() {
   $('div.dark-gray').children('h2.orange.center-text').prepend('<a href = "mailto:signup@gmawji.com?Subject=Lets%20Connect" target="_top"> Let\'s Connect </a>');
 }
 $(mailConnect);
+
+function projectLinks() {
+  $('div.education-entry').next().next().next().children('a').attr('href', 'https://www.udacity.com/course/front-end-web-developer-nanodegree--nd001')[0];
+
+  $('div.education-entry').next().next().next().children('a').attr('target', '_blank')[0];
+
+  $('div.education-entry').next().next().next().children('a').attr('href', 'https://www.udacity.com/course/front-end-web-developer-nanodegree--nd001')[1];
+
+  $('div.education-entry').next().next().next().children('a').attr('target', '_blank')[1];
+
+  $('div.education-entry').next().next().next().children('a').attr('href', 'https://www.udacity.com/course/full-stack-web-developer-nanodegree--nd004')[2];
+
+  $('div.education-entry').next().next().next().children('a').attr('target', '_blank')[2];
+
+  $('div.education-entry').next().next().next().children('a').attr('href', 'https://www.udacity.com/course/full-stack-web-developer-nanodegree--nd004')[3];
+
+  $('div.education-entry').next().next().next().children('a').attr('target', '_blank')[3];
+}
+$(projectLinks);
