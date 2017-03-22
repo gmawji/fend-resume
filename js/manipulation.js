14a2a1a227fd4c90ca555f4adbbded3549f08414
gmawji <git@gmawji.com>
2017-03-21 23:01:03 -0400

    Added projectLinks. The real project links lol.

--- js/manipulation.js
+++ js/manipulation.js
@@ -31,3 +31,16 @@ function educationLinks() {
   $('div.education-entry').next().next().next().children('a').attr('target', '_blank')[3];
 }
 $(educationLinks);
+
+function projectLinks() {
+  var initialize = $('#projects').children('div.project-entry').children('a');
+
+  initialize.attr('href', 'https://github.com/gmawji/Build-A-Portfolio-Website-Project').attr('target', '_blank')[0];
+
+  initialize.attr('href', 'https://github.com/gmawji/tournament-project-submit').attr('target', '_blank')[1];
+
+  initialize.attr('href', 'https://github.com/gmawji/multi-user-blog').attr('target', '_blank')[2];
+
+  initialize.attr('href', 'https://github.com/gmawji/Movie-Trailer-Website-Project').attr('target', '_blank')[3];
+}
+$(projectLinks);
